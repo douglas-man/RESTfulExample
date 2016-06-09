4e1493d860d552d24becee7759185b4ac4801eb3
Douglas Man <douglas.man@me.com>
2016-06-09 16:13:06 -0400

    log4j.properties

--- src/main/java/com/webservices/rest/HelloWorldService.java
+++ src/main/java/com/webservices/rest/HelloWorldService.java
@@ -1,5 +1,8 @@
 package com.webservices.rest;
  
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,11 +10,14 @@ import javax.ws.rs.core.Response;
  
 @Path("/hello")
 public class HelloWorldService {
+
+	private Log log = LogFactory.getLog(HelloWorldService.class);
  
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
- 
+        log.info("received request for hello");
+
 		String output = "Jersey say : " + msg;
  
 		return Response.status(200).entity(output).build();

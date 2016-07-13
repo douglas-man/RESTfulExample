a66cf6e06bfa61d2a36556673adc399435910d05
douglas man <douglas.man@me.com>
2016-07-13 17:44:16 -0400

    Log4j ...

--- src/main/java/com/webservices/rest/HelloWorldService.java
+++ src/main/java/com/webservices/rest/HelloWorldService.java
@@ -1,33 +1,36 @@
 package com.webservices.rest;
  
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+
+import org.apache.log4j.Logger;
+
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
+//import java.util.logging.Level;
+//import java.util.logging.LogRecord;
+//import java.util.logging.Logger;
  
 @Path("/hello")
 public class HelloWorldService {
-	private static final Logger LOG = LogUtils.getLogger(HelloWorldService.class);
+	private static final Logger LOG = Logger.getLogger(HelloWorldService.class);
 
-	private Log log = LogFactory.getLog(HelloWorldService.class);
+//	private Log log = LogFactory.getLog(HelloWorldService.class);
  
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
 
-		log.debug("getMsg() is executed!");
-		log.info("getMsg() is executed!");
-		log.warn("getMsg() is executed!");
-		log.error("getMsg() is executed!");
+		LOG.debug("getMsg() is executed!");
+		LOG.info("getMsg() is executed!");
+		LOG.warn("getMsg() is executed!");
+		LOG.error("getMsg() is executed!");
 
-		LOG.log(new LogRecord(Level.INFO, "Jersey say"));
+//		LOG.log(new LogRecord(PlatformLogger.Level.INFO, "Jersey say"));
 
 		String output = "Jersey say : " + msg;
  

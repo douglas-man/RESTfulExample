e885a7cbe66878f37a30d8794e3385df7643fe19
Douglas Man <douglas.man@me.com>
2017-01-17 22:00:04 -0500

    ..logger...

--- src/main/java/com/webservices/rest/HelloWorldService.java
+++ src/main/java/com/webservices/rest/HelloWorldService.java
@@ -1,20 +1,22 @@
 package com.webservices.rest;
  
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
+
+//import java.util.logging.Level;
+//import java.util.logging.LogRecord;
+//import java.util.logging.Logger;
  
 @Path("/hello")
 public class HelloWorldService {
-	private static final Logger LOG = LogUtils.getLogger(HelloWorldService.class);
+	private static final Logger LOG = Logger.getLogger(HelloWorldService.class);
 
 	private Log log = LogFactory.getLog(HelloWorldService.class);
  
